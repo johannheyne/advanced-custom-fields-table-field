8f29df03dd89e5c2bc999a31dd25faabf592b96d
johannheyne <mail@johannheyne.de>
2016-04-27 08:22:54 +0200

    FIX returning false on single empty cell for v4

--- advanced-custom-fields-table-field/trunk/acf-table-v4.php
+++ advanced-custom-fields-table-field/trunk/acf-table-v4.php
@@ -209,6 +209,17 @@
 				// BODY
 
 				$value['body'] = $a['b'];
+
+				// IF SINGLE EMPTY CELL, THEN DO NOT RETURN TABLE DATA
+
+				if ( 
+					count( $a['b'] ) === 1
+					AND count( $a['b'][0] ) === 1
+					AND trim( $a['b'][0][0]['c'] ) === ''
+				) {
+
+					$value = false;
+				}
 			}
 
 			return $value;
